14a8702e1eb4f2a5decb6791eed0102bc84a587f
Phan Trung Kiên <trungkien.it.98@gmail.com>
2020-05-03 02:02:12 +0700

    Add README

--- models/call.js
+++ models/call.js
@@ -1,36 +1,37 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
+// Phần này chưa sửa dụng vì có thể lấy log cuộc gọi từ Stringee
+// Hướng dẫn ở README
+
 var CallSchema = new Schema({
-    actorType: String,
-    original: Boolean,
-    peerToPeer: Boolean,
-    timestamp_ms: Number,
-    customData: String,
-    type: String,
-    call_id: String,
-    actor: String,
-    callCreatedReason: String,
-    call_status: String,
-    event_id: String,
-    project_id: Number,
-    serial: Number,
-    account_sid: String,
-    from:
-    {
-        number: String,
-        alias: String,
-        is_online: false,
-        type: String
-    },
-    to:
-    {
-        number: String,
-        alias: String,
-        is_online: false,
-        type: String
-    },
-    direction: String
+	actorType: String,
+	original: Boolean,
+	peerToPeer: Boolean,
+	timestamp_ms: Number,
+	customData: String,
+	type: String,
+	call_id: String,
+	actor: String,
+	callCreatedReason: String,
+	call_status: String,
+	event_id: String,
+	project_id: Number,
+	serial: Number,
+	account_sid: String,
+	from: {
+		number: String,
+		alias: String,
+		is_online: false,
+		type: String,
+	},
+	to: {
+		number: String,
+		alias: String,
+		is_online: false,
+		type: String,
+	},
+	direction: String,
 });
 
-module.exports = mongoose.model('Call', CallSchema);
+module.exports = mongoose.model("Call", CallSchema);
